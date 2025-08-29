c531dcbedfa602a01537bb50c47039a59f78560c
kazeemmuyideen <kazeemmuyideen@gmail.com>
2025-08-29 23:13:53 +0100

    1. Create Your First Mobile App

---
+++ prodev-mobile-app-0x00/app/(tabs)/index.tsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>** First App Created**</Text>
+
+      <View style={styles.inner}>
+        <Text style={styles.largeText}>
+          Typescript is great if you practice more
+        </Text>
+        <Text style={styles.mediumText}>
+          React Native provides you a single codebase for cross platforms
+        </Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#fff", padding: 20, justifyContent: "center" },
+  inner: { marginTop: 12 },
+  largeText: { fontSize: 30, color: "#f44336", marginBottom: 5, fontWeight: "700" },
+  mediumText: { fontSize: 20, color: "#9c27b0", marginBottom: 10, fontWeight: "500", textAlign: "right" },
+  smallText: { fontSize: 15, color: "#2196f3", fontWeight: "400", textAlign: "center" },
+});
